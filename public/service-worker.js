b1b3c6331247e6459f0dc8452432b7338961843b
Asher <asher.bergtraun@gmail.com>
2021-09-30 08:36:48 -0700

    fixed files to cache typo

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ const FILES_TO_CACHE = [
     'icons/icon-96x96.png',
     'icons/icon-128x128.png',
     'icons/icon-144x144.png',
-    'icons/icon-152x72.png',
+    'icons/icon-152x152.png',
     'icons/icon-192x192.png',
     'icons/icon-384x384.png',
     'icons/icon-512x512.png'
@@ -25,7 +25,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache: ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE);
         })
         .catch(err => {
             console.log(err);

357a35c287a9678195d3eeaf5edc1eb325ce167f
Asher <asher.bergtraun@gmail.com>
2021-09-30 07:15:34 -0700

    added onupgrade function in idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,3 +4,13 @@ let db;
 //contect to indexedDB database "budget_tracker" version 1
 const request = indexedDB.open('budget_tracker', 1);
 
+//execute function if database version changed
+request.onupgradeneeded = function(event) {
+
+    //save reference to previous database version
+    const db = event.target.result;
+
+    //Create object store with an auto incrementing primary key
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+

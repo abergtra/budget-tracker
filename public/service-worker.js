302d4fd9d1e6fba110cf5c8192ea86c8c464a793
Asher <asher.bergtraun@gmail.com>
2021-09-30 06:38:29 -0700

    added event listener to install cace in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,13 +10,22 @@ const FILES_TO_CACHE = [
     './js/index.js',
     './js/idb.js',
     './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    'icons/icon-72x72.png',
+    'icons/icon-96x96.png',
+    'icons/icon-128x128.png',
+    'icons/icon-144x144.png',
+    'icons/icon-152x72.png',
+    'icons/icon-192x192.png',
+    'icons/icon-384x384.png',
+    'icons/icon-512x512.png'
 ];
 
+//event listener to install cache
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
